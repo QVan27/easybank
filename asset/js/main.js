39d893a0516419e51009bebad2960ad180d579d4
QVan27 <qvannarath@gmail.com>
2021-04-12 17:37:28 +0200

    menu burger

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,4 +1,16 @@
+// locomotive Scroll
+
 const scroller = new LocomotiveScroll({
   el: document.querySelector("[data-scroll-container]"),
   smooth: true,
+});
+
+// Menu Burger
+const menu = document.querySelector(".menu");
+const burger = document.querySelector(".burger");
+
+burger.addEventListener("click", () => {
+  menu.classList.toggle("active");
+  burger.classList.toggle("active");
+  // console.log(burger)
 });
